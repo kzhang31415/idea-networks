4bce4bb0bd743652867abcc4166b76f48fe1d2e7
shoefeng1 <clairechair510@gmail.com>
2024-02-03 13:09:07 -0500

    first draft

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,116 @@
-from flask import Flask
+# from flask import Flask
+# from random_word import RandomWords
+# import os
+# nltk.download('gutenberg')
+# nltk.download('wordnet')
+import csv
+import nltk
+from nltk.corpus import gutenberg
+from nltk.corpus import wordnet as wn
+import random
+import pandas as pd
+from openai import OpenAI
+import networkx as nx
+import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key = "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",
+)
+# client = OpenAI()
+def genresponse(prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return chat_completion.choices[0].message.content
+
+with open("moby1.csv", "r") as moby_file:
+    csv_reader = csv.reader(moby_file)
+    next(csv_reader)
+    data_words = [line[1] for line in csv_reader]
+    # print(random.choice([line[1] for line in csv_reader]))
+    
+def random_word():
+    return random.choice(data_words)
+
+def simplify(s):
+    p = "Can you summarize the most important concept of the phrase \""
+    q = "\"? Please respond with one word."
+    s_chat = genresponse(p + s + q)
+    return ''.join([c.lower() for c in s_chat if c.isalnum()])
+    
+def get_graph(s1, s2):
+    wG = nx.Graph()
+    si = simplify(s1)
+    target = simplify(s2)
+    print(si)
+    print(target)
+    threshold = 0.9
+    
+    wG.add_nodes_from([
+        (si, {'synset' : wn.synsets(si)[0]}),
+        (target, {'synset' : wn.synsets(target)[0]})
+    ])
+    
+    while not nx.is_connected(wG):
+        w = random_word()
+        connect = False
+        # you want to check w's similarity against each word in the graph wG
+        print(w)
+        if len(wn.synsets(w)) == 0:
+            continue
+        w_set = wn.synsets(w)[0]
+        wG.add_node(w, synset=w_set)
+        
+        for word in wG.nodes():
+            score = w_set.path_similarity(wG.nodes[word]['synset'])
+            if score > threshold:
+                connect = True
+                wG.add_edge(w, word)
+        
+        if not connect:
+            wG.remove_node(w)
+        else:
+            wG.remove_edge(w, w)
+        
+    nx.draw(wG)
+    plt.show()
+    
+    return wG
+
+
+word_graph_test = get_graph('jazz', 'pie')
+
+# prompt = "What's the most popular ski resort in Europe?"
+# sample = genresponse("What's the most popular ski resort in Europe?")
+# print(sample.choices[0].message.content)
+
+
+# print(gen_response("Write a summary of the benefits of exercise."))
+
+# app = Flask(__name__)
 
 # Members API route
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+# @app.route("/members")
+# def members():
+#     return {"members": ["Member1", "Member2", "Member3"]}
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# r = RandomWords()
+# Return a single random word
+# print(r.get_random_word())
+
+# moby = set(nltk.Text(gutenberg.words('melville-moby_dick.txt')))
+# moby = [word.lower() for word in moby if len(word) >2]
+# moby_df = pd.DataFrame(moby)
+
+# moby_df.to_csv('moby1.csv')
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
