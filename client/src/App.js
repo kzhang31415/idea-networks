3565159bd3b6853b59df293c5bbeb484c96006d0
Kevin Zhang <kz3@andrew.cmu.edu>
2024-02-03 12:38:08 -0500

    Implemented input method, fixed api stuff from frontend side

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import data from './data/data.json';
+import data from './data/data.json'; //Remove this once the backend is set up
 import { ForceGraph } from "./components/forceGraph";
 import './App.css';
 
 function App() {
   // const [data, setData] = useState([{}]);
-  // useEffect(() => {
-  //     fetch('/members').then(
-  //       res => res.json()
-  //     ).then(
-  //       data => {
-  //         setData(data);
-  //         console.log(data);
-  //       }
-  //     )
-  // }, []); //Once this is uncommented, the data.json file will not be used and the data will be fetched from the server
+  const [input, setInput] = useState('');
 
+  const handleTextareaChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    //fetch('/api/make-graph?input=' + input).then(
+    //  res => res.json()
+    //).then(
+    //  data => {
+    //  setData(data);
+    //  console.log(data);
+    //})
+  }
 
   const nodeHoverTooltip = React.useCallback((node) => {
     return `<div>${node.name}</div>`;
@@ -24,10 +30,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Force Graph Example 
+        <h1>Idea Networks</h1>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={input}
+            onChange={handleTextareaChange}
+          />
+          <br></br>
+          <button type="submit">Make graph!</button>
+        </form>
       </header>
       <section className="Main">
-        <ForceGraph linksData={(typeof data.links === 'undefined') ? {} : data.links} nodesData={(typeof data.links === 'undefined') ? {} : data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
+        <ForceGraph linksData={(typeof data === 'undefined') ? {} : data.links} nodesData={(typeof data === 'undefined') ? {} : data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
       </section>
     </div>
   );
