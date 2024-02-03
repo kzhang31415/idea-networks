e254480bfb5f3d774048e581f784899747ab9488
Kevin Zhang <kz3@andrew.cmu.edu>
2024-02-02 22:22:00 -0500

    Graphs are up

---
+++ react-d3-force/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import data from './data/data.json';
+import { ForceGraph } from "./components/forceGraph";
+import './App.css';
+
+function App() {
+  const nodeHoverTooltip = React.useCallback((node) => {
+    return `<div>${node.name}</div>`;
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Force Graph Example
+      </header>
+      <section className="Main">
+        <ForceGraph linksData={data.links} nodesData={data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
