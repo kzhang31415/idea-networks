8f54b6683a9792c906fb1d1b59285f044827cda0
Kevin Zhang <kz3@andrew.cmu.edu>
2024-02-03 02:16:43 -0500

    Implemented API data fetching

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,19 @@ import { ForceGraph } from "./components/forceGraph";
 import './App.css';
 
 function App() {
+  // const [data, setData] = useState([{}]);
+  // useEffect(() => {
+  //     fetch('/members').then(
+  //       res => res.json()
+  //     ).then(
+  //       data => {
+  //         setData(data);
+  //         console.log(data);
+  //       }
+  //     )
+  // }, []); //Once this is uncommented, the data.json file will not be used and the data will be fetched from the server
+
+
   const nodeHoverTooltip = React.useCallback((node) => {
     return `<div>${node.name}</div>`;
   }, []);
@@ -11,10 +24,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Force Graph Example
+        Force Graph Example 
       </header>
       <section className="Main">
-        <ForceGraph linksData={data.links} nodesData={data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
+        <ForceGraph linksData={(typeof data.links === 'undefined') ? {} : data.links} nodesData={(typeof data.links === 'undefined') ? {} : data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
       </section>
     </div>
   );
